078eb506dbc9eb299723d4b4478e13e1735099ea
Pawel Kot <mj4u961@gmail.com>
2022-06-20 15:36:07 +0200

    gallery1

---
+++ gallery/main.js
@@ -0,0 +1,28 @@
+// function myFunction(id) {
+//     const reg = ".{7}$";
+//     console.log("myfunciton , id: " + id);
+//     console.log("myfunciton , src: " + document.getElementById(id).src.replace(reg, "500/350"));
+//     //document.getElementById(id).;
+//   }
+
+const thumbnails = document.querySelectorAll("img");
+//const thumbnail1 = thumbnails[1];
+
+//console.log("thumbnail: ", thumbnails);
+
+// console.log(
+//   "thumbnail1: ",
+//   thumbnails[1].src.replace(new RegExp(".{7}$"), "750/500")
+// );
+const outlet = document.querySelector("#outlet");
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", () => {
+    console.log("thumbnail: ", thumbnail);
+    const biggerSource = thumbnail.src.replace(new RegExp(".{7}$"), "450/300");
+    console.log("bigger: ", biggerSource);
+    if (outlet) {
+      outlet.innerHTML = `<img src="${biggerSource}" alt=""/>`;
+    }
+  });
+});
