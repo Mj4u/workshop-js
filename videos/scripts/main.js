e9a89a21ea698f80f71a96045d0251448880df34
Pawel Kot <mj4u961@gmail.com>
2022-06-21 12:05:28 +0200

    add routing mechanism

--- videos/scripts/main.js
+++ videos/scripts/main.js
@@ -1,22 +1,55 @@
-function copyHomepageTemplate() {
+
+function copyTemplate(selector) {
   /**
    * @type {HTMLTemplateElement|null}
    */
-  const homepageTemplate = document.querySelector("#homepage");
-  return homepageTemplate?.content.cloneNode(true);
+  const template = document.querySelector(selector);
+  return template?.content.cloneNode(true);
+}
+
+function render(page) {
+  if (!page) {
+    throw new Error("Page is not defined");
+  }
+  console.log("render page", page);
+  const outlet = document.querySelector("#outlet");
+  if (!outlet) {
+    throw new Error("outlet is not defined");
+  }
+
+  while (outlet.firstElementChild) {
+    outlet.firstElementChild.remove();
+  }
+  outlet.append(page);
+}
+
+function renderPage(selector) {
+  render(copyTemplate(selector));
+}
+
+function renderApp() {
+  switch (location.hash) {
+    case "":
+    case "#/":
+      renderPage("#homepage");
+      break;
+
+    case "#/registration":
+      renderPage("#registration");
+      break;
+
+    case "#/login":
+      renderPage("#login");
+      break;
+    default:
+      console.log("ups... 404");
+      break;
+  }
 }
 
 function main() {
-  const outlet = document.querySelector("#outlet");
-  const homepage = copyHomepageTemplate();
-  if (homepage) {
-    outlet?.append(homepage);
-  }
-
-  console.log("homepage: ");
+  window.addEventListener("hashchange", renderApp);
+  renderApp;
 }
 
-//main();
-
 window.addEventListener("DOMContentLoaded", main);
-// homepage.content
