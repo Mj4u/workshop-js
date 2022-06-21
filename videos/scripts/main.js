7953720fe003d2984acc808e4f8a7d6b9957762d
Pawel Kot <mj4u961@gmail.com>
2022-06-21 10:39:53 +0200

    use template to display videos

--- videos/scripts/main.js
+++ videos/scripts/main.js
@@ -1,1 +1,22 @@
-console.debug("loooog");
+function copyHomepageTemplate() {
+  /**
+   * @type {HTMLTemplateElement|null}
+   */
+  const homepageTemplate = document.querySelector("#homepage");
+  return homepageTemplate?.content.cloneNode(true);
+}
+
+function main() {
+  const outlet = document.querySelector("#outlet");
+  const homepage = copyHomepageTemplate();
+  if (homepage) {
+    outlet?.append(homepage);
+  }
+
+  console.log("homepage: ");
+}
+
+//main();
+
+window.addEventListener("DOMContentLoaded", main);
+// homepage.content
