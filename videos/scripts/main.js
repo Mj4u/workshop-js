4099900b2c9c8a55bb282ff076b8101fa414c8e9
Pawel Kot <mj4u961@gmail.com>
2022-06-22 08:22:23 +0200

    add registration complete screen

--- videos/scripts/main.js
+++ videos/scripts/main.js
@@ -1,55 +1,13 @@
 
-function copyTemplate(selector) {
-  /**
-   * @type {HTMLTemplateElement|null}
-   */
-  const template = document.querySelector(selector);
-  return template?.content.cloneNode(true);
-}
+// function 
 
-function render(page) {
-  if (!page) {
-    throw new Error("Page is not defined");
-  }
-  console.log("render page", page);
-  const outlet = document.querySelector("#outlet");
-  if (!outlet) {
-    throw new Error("outlet is not defined");
-  }
+//function 
 
-  while (outlet.firstElementChild) {
-    outlet.firstElementChild.remove();
-  }
-  outlet.append(page);
-}
 
-function renderPage(selector) {
-  render(copyTemplate(selector));
-}
-
-function renderApp() {
-  switch (location.hash) {
-    case "":
-    case "#/":
-      renderPage("#homepage");
-      break;
-
-    case "#/registration":
-      renderPage("#registration");
-      break;
-
-    case "#/login":
-      renderPage("#login");
-      break;
-    default:
-      console.log("ups... 404");
-      break;
-  }
-}
 
 function main() {
   window.addEventListener("hashchange", renderApp);
-  renderApp;
+  renderApp();
 }
 
 window.addEventListener("DOMContentLoaded", main);
